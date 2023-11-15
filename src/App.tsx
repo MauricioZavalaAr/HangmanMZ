94513c7b1b670006efc2f4dd05f3582639da0aa7
MauricioZavalaAr <mauricio.zavala.ar@gmail.com>
2023-11-14 21:28:19 -0600

    test4

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ const App: React.FC = () => {
       className={isWon ? 'dark-blue-background' : ''}
     >
       <img
-        src={hS}
+        src={`../src/images/hangman0.jpg`}
         alt={`Hangman Stage ${hangmanStage}`}
         className="hangman-image"
       />
